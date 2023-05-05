563165491d380871d6e7c18a4e2a399f5b83eb25
Arthur <91443695+ArthurAsam@users.noreply.github.com>
2023-05-05 18:13:47 -0500

    Add game function, this is part two of my program

--- RPS.js
+++ RPS.js
@@ -2,10 +2,10 @@
 const cpuOptions = ["rock", "paper", "scissors"]; 
 
 
- //get random selection from computer
+ //function to get random selection from the array above
 function getComputerChoice() {
     
-                // length of the array is 3 so * 3 would also work
+                
     const randomOption = cpuOptions[Math.floor(Math.random() * cpuOptions.length)]
     
     return randomOption; 
@@ -15,45 +15,72 @@ function getComputerChoice() {
 
 let computerSelection = getComputerChoice();
 
-//Allows selection to be case insensitive 
-let playerSelection = prompt("Choose: rock, paper, or scissors", "").toLowerCase(); 
 
+let playerSelection = prompt("Choose: rock, paper, or scissors", "").toLowerCase(); 
+let playerCounter = 0; 
+let cpuCounter = 0; 
         
 
         function playRound(playerSelection, computerSelection) {
             
-            //Highlight the tiebreakers
+           
             if (computerSelection === "rock" && playerSelection === "rock"){
                 return 'You tied! You both picked rock'
             } else if (computerSelection === "rock" && playerSelection === "paper") {
+                playerCounter++
                 return 'You Won! Paper beats rock All Day'
             } else if (computerSelection === "rock" && playerSelection === "scissors") {
-                return 'You lost! Opponents rock smashes your scissors'
+                cpuCounter++
+                return 'You lose! Opponents rock smashes your scissors'
             } else if (computerSelection === "paper" && playerSelection === "rock") {
+                cpuCounter++
                 return 'You lose! Opponents paper covers your rock'
-            //Highlight ties!
+            
             } else if (computerSelection === "paper" && playerSelection === "paper") {
                 return 'You tied! You both chose paper'
             } else if (computerSelection === "paper" && playerSelection === "scissors"){
-                return 'You Won! Your scissors sliced their paper' 
+                playerCounter++
+                return 'You Won! Your scissors sliced their paper'                 
             } else if (computerSelection === "scissors" && playerSelection === "rock") {
-                return 'You Won! Your rock smashed their scissors' 
+                playerCounter++
+                return 'You Won! Your rock smashed their scissors'                 
             } else if (computerSelection === "scissors" && playerSelection === "paper") {
-                return 'You lose! Opponents scissors gave you the slice N\' dice'
-                //Highlight ties!
+                cpuCounter++
+                return 'You lose! Opponents scissors gave you the slice N\' dice'                
+                
             } else if (computerSelection === "scissors" && playerSelection === "scissors") {
                 return 'You tied! You both chose scissors'
-                //Else statement accounts for unverifiable user inputs
+               
             } else {
                 return 'Error'
             }
         }
-        //calls function and prints to console any returns
-        console.log(playRound(playerSelection, computerSelection))
+     
+        playRound(playerSelection, computerSelection)
 
 // Part One of the RPS game
 
 
 
+
+
+//part two function, run a 5 game simulation 
+
+const game = () => {
+    for (let i = 0; i < 5; i++){
+        let computerSelection = getComputerChoice()
+        playRound(playerSelection, computerSelection)
+    }
+
+    if (playerCounter > cpuCounter){
+        return `Winner Winner Chicken Dinner! You won ${playerCounter} out of 5 times`
+    } else if (cpuCounter > playerCounter){
+        return `You lost this time...Bummer. You lost CPU beat you ${cpuCounter} out of 5 times` 
+    } else {
+        return 'It\'s a draw...let\'s play again!'
+    }
+}
+
+console.log(game())
 
 
